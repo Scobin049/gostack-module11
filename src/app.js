28064e9e0849fab3d6b579dd60b496efa1c00d52
Vitor Hugo Scobin <vhscobin@gmail.com>
2019-08-02 18:42:39 -0300

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import './bootstrap';
 
 import Youch from 'youch';
 import express from 'express';
@@ -7,8 +7,7 @@ import 'express-async-errors';
 import routes from './routes';
 
 // Uncomment this line to enable database access
-// --------
-// import './database';
+import './database';
 
 class App {
   constructor() {
